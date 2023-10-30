a7381631a29440b983ac62513397ac2fff87f54c
Joshua Newton <joshuacwnewton@gmail.com>
2023-10-30 14:54:44 -0400

    `batch_single_subject.sh`: Update registration command

--- single_subject/batch_single_subject.sh
+++ single_subject/batch_single_subject.sh
@@ -167,12 +167,11 @@ sct_deepseg -i t2_crop.nii.gz -task seg_lumbar_sc_t2w
 sct_label_utils -i t2.nii.gz -create 22,77,187,17:27,79,80,60 -o t2_crop_labels.nii.gz
 
 # Register the image to the template using segmentation and labels
-# TODO: Uncomment when https://github.com/spinalcordtoolbox/PAM50/pull/27 gets merged and a new PAM50 release is made.
-#sct_register_to_template -i t2_crop.nii.gz \
-#                         -s t2_crop_seg.nii.gz \
-#                         -ldisc t2_crop_label.nii.gz \
-#                         -c t2 -qc qc \
-#                         -param step=1,type=seg,algo=centermassrot:step=2,type=seg,algo=bsplinesyn,metric=MeanSquares,iter=3,slicewise=0:step=3,type=im,algo=syn,metric=CC,iter=3,slicewise=0
+sct_register_to_template -i t2_crop.nii.gz \
+                         -s t2_crop_seg.nii.gz \
+                         -ldisc t2_crop_label.nii.gz \
+                         -c t2 -qc qc \
+                         -param step=1,type=seg,algo=centermassrot:step=2,type=seg,algo=bsplinesyn,metric=MeanSquares,iter=3,slicewise=0:step=3,type=im,algo=syn,metric=CC,iter=3,slicewise=0
 
 
 
