cd149daa882bd00173ffbed484f3260450db1485
Joshua Newton <joshuacwnewton@gmail.com>
2025-11-19 14:22:25 -0500

    `batch_single_subject.sh`: Fix `aSCOR` typo (`-1` -> `1`)

--- single_subject/batch_single_subject.sh
+++ single_subject/batch_single_subject.sh
@@ -438,7 +438,7 @@ fsleyes t2.nii.gz -cm greyscale t2_canal_seg_seg.nii.gz -cm red -a 70.0 &
 
 # Compute aSCOR (Adapted Spinal Cord Occupation Ratio)
 # i.e. Spinal cord to canal ratio using the canal seg
-sct_compute_ascor -i-SC t2_seg.nii.gz -i-canal t2_canal_seg.nii.gz -perlevel -1 -o ascor.csv
+sct_compute_ascor -i-SC t2_seg.nii.gz -i-canal t2_canal_seg.nii.gz -perlevel 1 -o ascor.csv
 
 # Segment the spinal nerve rootlets
 sct_deepseg rootlets -i t2.nii.gz -qc ~/qc_singleSubj
